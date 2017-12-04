4026b54cc34d4b8aef65f6dfc0d1f731211c22c7
Tymon Tobolski <i@teamon.eu>
2017-12-04 15:34:43 +0100

    nodejs adjustments

--- builder/build.sh
+++ builder/build.sh
@@ -3,10 +3,25 @@
 set -e
 
 tool="$1"
-version="$2"
+src_version="$2"
+dst_version="$2"
 
 asdf plugin-add $tool
-asdf install $tool $version
+
+# tool-specific adjustments
+case $tool in
+  "nodejs")
+    export NODEJS_CHECK_SIGNATURES=no
+    src_version="ref:v$src_version"
+    ;;
+
+  *)
+    ;;
+esac
+
+echo "==> Building $tool $src_version => $dst_version"
+
+asdf install $tool $src_version
 
 mkdir -p /build/$tool
-mv /asdf/.asdf/installs/$tool/$version /build/$tool/$version
+mv /asdf/.asdf/installs/$tool/$src_version /build/$tool/$dst_version
